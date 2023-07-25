66f60084c6572d8b56625007c6e4669dc7519806
MaheshMERN <88592368+MaheshMERN@users.noreply.github.com>
2023-07-25 21:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArt(bandName){
+   return bandName.replace(/^(a|an|the)/i, "".trim())
+}
+
+const sortedBand = touristSpots.sort((a,b)=>removeArt(a) > removeArt(b) ? 1 : -1);
+
+document.getElementById("band").innerHTML = sortedBand.map(band => `<li>${band}</li>`).join("")
